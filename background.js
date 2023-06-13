e8bd317bdb73be51cb68d4129169a0f0432d1715
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-06-13 17:16:15 -0400

    reverting/updating for gpt

--- background.js
+++ background.js
@@ -1,16 +1,61 @@
-// Listener for when the user clicks the extension's icon
-chrome.action.onClicked.addListener((tab) => {
-  // Inject the content script into the current page
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ['content.js']
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: tabId },
+        files: ['content.js'],
+        runAt: 'document_idle',
+      },
+      (result) => {
+        if (chrome.runtime.lastError) {
+          console.error(chrome.runtime.lastError);
+          return;
+        }
+        const conversations = result[0].result;
+        console.log(
+          'Received conversations from content script:',
+          conversations
+        );
+        chrome.storage.local.set({ conversations });
+      }
+    );
+  }
+});
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  chrome.tabs.get(activeInfo.tabId, (tab) => {
+    if (/^http/.test(tab.url)) {
+      getConversations(tab.id);
+    }
   });
 });
 
-// Listener for messages from the content script
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'CONVERSATIONS') {
-    // Save the conversations to chrome.storage
-    chrome.storage.local.set({ conversations: request.data });
-  }
-});
+function getConversations(tabId) {
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tabId },
+      function: fetchConversations,
+    },
+    (result) => {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError);
+        return;
+      }
+      const conversations = result[0].result;
+      console.log('Received conversations from content script:', conversations);
+      chrome.storage.local.set({ conversations });
+    }
+  );
+}
+
+function fetchConversations() {
+  const conversations = Array.from(
+    document.querySelectorAll(
+      '.flex-1.text-ellipsis.max-h-5.overflow-hidden.break-all.relative'
+    )
+  ).map((el, index) => ({
+    name: el.textContent,
+    position: index,
+  }));
+  return conversations;
+}
--- popup.js
+++ popup.js
@@ -1,15 +1,66 @@
 // Fetch the conversations from chrome.storage
 chrome.storage.local.get('conversations', (data) => {
+  console.log('Data retrieved from chrome.storage:', data);
   // Get the conversations array from the data
-  let conversations = data.conversations;
+  let conversations = data.conversations || [];
 
   // Get the conversations list element
   let conversationsList = document.getElementById('conversations');
 
-  // Create a list item for each conversation and append it to the list
-  conversations.forEach((conversation) => {
-    let listItem = document.createElement('li');
-    listItem.textContent = conversation.name;
-    conversationsList.appendChild(listItem);
+  // Get the folders container element
+  let foldersContainer = document.getElementById('folders-container');
+
+  // Create dynamic folders
+  const folders = ['Folder 1', 'Folder 2', 'None']; // Replace with your dynamic folder data
+  folders.forEach((folderName) => {
+    let folder = document.createElement('div');
+    folder.className = 'folder';
+    folder.textContent = folderName;
+    folder.addEventListener('dragover', (e) => {
+      e.preventDefault();
+      e.dataTransfer.dropEffect = 'move';
+    });
+
+    folder.addEventListener('drop', (e) => {
+      e.preventDefault();
+      const conversationData = JSON.parse(
+        e.dataTransfer.getData('text/custom')
+      );
+      const newFolder = folder.textContent.trim();
+
+      if (newFolder === 'None') {
+        conversationData.folder = null;
+      } else {
+        conversationData.folder = newFolder;
+      }
+
+      const conversationElement = document.createElement('li');
+      conversationElement.textContent = conversationData.name;
+      conversationElement.draggable = true; // Enable dragging
+      conversationElement.setAttribute('data-folder', conversationData.folder); // Set folder attribute
+      conversationsList.appendChild(conversationElement);
+
+      // Add event listener for drag start
+      conversationElement.addEventListener('dragstart', (event) => {
+        event.dataTransfer.setData('text/plain', conversationData.name);
+        event.dataTransfer.effectAllowed = 'move';
+        event.dataTransfer.setData(
+          'text/custom',
+          JSON.stringify(conversationData)
+        ); // Store conversation data
+      });
+
+      chrome.storage.local.get('conversations', (data) => {
+        const savedConversations = data.conversations || [];
+        savedConversations.push(conversationData);
+        chrome.storage.local.set({ conversations: savedConversations });
+      });
+
+      e.target.querySelector('ul').appendChild(conversationElement);
+    });
+
+    let folderList = document.createElement('ul');
+    folder.appendChild(folderList);
+    foldersContainer.appendChild(folder);
   });
 });
