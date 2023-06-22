0d4ea26ad5082b4539bd7b6f7cc463ddb7b73adb
DrBretto <59483898+DrBretto@users.noreply.github.com>
2023-06-22 11:50:37 -0400

    working drag and drop

--- popup.js
+++ popup.js
@@ -13,68 +13,92 @@ let conversationsWithFolders = [
   // Add more dummy conversations as needed
 ];
 
+function groupConversationsByFolder(conversations) {
+  console.log(conversations);
+  let grouped = {};
+  conversations.forEach((conversation) => {
+    if (!grouped[conversation.folder]) {
+      grouped[conversation.folder] = [];
+    }
+    grouped[conversation.folder].push(conversation);
+  });
+  return grouped;
+}
+
 function updateConversations() {
-  // Get the conversations container element
-  let conversationsContainer = document.getElementById('conversations');
-  conversationsContainer.innerHTML = ''; // Clear the container
+  // Get the conversations list element
+  let conversationsList = document.getElementById('conversations');
+  conversationsList.innerHTML = ''; // Clear the list
 
-  // Fetch the conversations from chrome.storage
-  chrome.storage.local.get('conversations', (data) => {
-    console.log('Data retrieved from chrome.storage:', data);
-    let conversations = data.conversations || [];
-    console.log('should be array', conversations);
+  // Replace the data from chrome.storage with dummy data
+  let conversations = conversationsWithFolders;
 
-    // Update conversations with folder information
-    conversations.forEach((conversation) => {
-      let matchingConversation = conversationsWithFolders.find(
-        (c) => c.name === conversation.name
-      );
-      if (matchingConversation) {
-        conversation.folder = matchingConversation.folder;
+  console.log('should be array', conversations);
+
+  // Group conversations by folder
+  let conversationsByFolder = groupConversationsByFolder(conversations);
+
+  // Display each folder
+  for (let folder in conversationsByFolder) {
+    // Create a list for the folder
+    let folderList = document.createElement('ul');
+    let folderLabel = document.createElement('h2');
+    folderLabel.textContent = folder;
+    folderList.appendChild(folderLabel);
+
+    // Add drop event listener
+    folderList.addEventListener('drop', (event) => {
+      event.preventDefault();
+      let name = event.dataTransfer.getData('text/plain');
+      let conversation = conversations.find((c) => c.name === name);
+      if (conversation) {
+        conversation.folder = folder;
+        updateConversations(); // Refresh the list
       }
     });
 
-    // Group conversations by folder
-    let conversationsByFolder = {};
-    conversations.forEach((conversation) => {
-      if (!conversationsByFolder[conversation.folder]) {
-        conversationsByFolder[conversation.folder] = [];
-      }
-      conversationsByFolder[conversation.folder].push(conversation);
+    // Add dragover event listener
+    folderList.addEventListener('dragover', (event) => {
+      event.preventDefault();
+      folderList.style.backgroundColor = 'lightgray'; // Change the appearance so the user knows a drop is possible
     });
 
-    // Display conversations
-    for (let folder in conversationsByFolder) {
-      // Create a list for the folder
-      let folderList = document.createElement('ul');
-      let folderLabel = document.createElement('h2');
-      folderLabel.textContent = folder;
-      folderList.appendChild(folderLabel);
+    // Add dragleave and dragend event listeners to reset appearance
+    folderList.addEventListener('dragleave', (event) => {
+      folderList.style.backgroundColor = ''; // Reset the appearance
+    });
+    folderList.addEventListener('dragend', (event) => {
+      folderList.style.backgroundColor = ''; // Reset the appearance
+    });
 
-      // Add conversations to the list
-      conversationsByFolder[folder].forEach((conversation) => {
-        let listItem = document.createElement('li');
-        listItem.textContent = conversation.name;
-        listItem.addEventListener('click', () => {
-          chrome.runtime.sendMessage({
-            type: 'CLICK_CONVERSATION',
-            position: conversation.position,
-          });
+    // Display conversations in this folder
+    conversationsByFolder[folder].forEach((conversation) => {
+      let listItem = document.createElement('li');
+      listItem.textContent = conversation.name;
+      listItem.draggable = true;
+      listItem.addEventListener('click', () => {
+        chrome.runtime.sendMessage({
+          type: 'CLICK_CONVERSATION',
+          position: conversation.position,
         });
-
-        folderList.appendChild(listItem);
       });
 
-      // Add the list to the container
-      conversationsContainer.appendChild(folderList);
-    }
-  });
+      // Add drag event listeners
+      listItem.addEventListener('dragstart', (event) => {
+        event.dataTransfer.setData('text/plain', conversation.name);
+      });
+
+      folderList.appendChild(listItem);
+    });
+
+    conversationsList.appendChild(folderList);
+  }
 }
 
 // Call the update function initially
 updateConversations();
 
-// Listen for changes in storage
+// Listen for changes in storage and update the list if conversations have changed
 chrome.storage.onChanged.addListener((changes, areaName) => {
   if (areaName === 'local' && changes.conversations) {
     // Conversations in storage have changed, update the list
